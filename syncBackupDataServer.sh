7599cca865eaf63b80efbfb89d11fb896f3c5dfc
hriverat <rivera.hector@outlook.es>
2022-06-26 12:32:29 +0200

    Add Sync shel File for Backup2

--- syncBackupDataServer.sh
+++ syncBackupDataServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rclone sync -v "/home/pi/Desktop/Setup DataServer/" "sharepointmv:General/08 - Softwares/McView DataServer/Setup DataServer"
-rclone sync -v "/home/pi/Desktop/DataHistorisatioAPP/Dictionaries" "sharepointmv:General/08 - Softwares/McView DataServer/Dictionaries"
+rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/Dictionaries" "sharepointmv:General/08 - Softwares/McView DataServer/Dictionaries"
