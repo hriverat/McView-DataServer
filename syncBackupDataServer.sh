b11c0e103dc729a1478c6bf7deab6e885a9f3799
hriverat <rivera.hector@outlook.es>
2022-06-26 23:32:24 +0200

    Paths updated

--- syncBackupDataServer.sh
+++ syncBackupDataServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/Dictionaries/Setup_McViewDataHistorisation.csv" "sharepointmv:General/08 - Softwares/McView DataServer/Setup DataServer"
-rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/Dictionaries" "sharepointmv:General/08 - Softwares/McView DataServer/Dictionaries"
-rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/IoT Gateway Status" "sharepointmv:General/08 - Softwares/McView DataServer/IoT Gateway Status"
+rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/Setup_McViewDataHistorisation.csv" "sharepointmv:General/08 - Softwares/McView DataServer/Input/Setup DataServer"
+rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/Dictionaries" "sharepointmv:General/08 - Softwares/McView DataServer/Input/Dictionaries"
+rclone sync -v "/home/pi/Desktop/DataHistorisationAPP/IoT Gateway Status" "sharepointmv:General/08 - Softwares/McView DataServer/Output/IoT Gateway Status"
